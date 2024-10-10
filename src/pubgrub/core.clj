937efc7de9055fa0ffb36444d6691d5cdc1dab86
theandrew168 <andrew@shallowbrooksoftware.com>
2024-10-09 21:53:59 -0500

    Add some basic version functions

--- src/pubgrub/core.clj
+++ src/pubgrub/core.clj
@@ -1,4 +1,5 @@
-(ns pubgrub.core)
+(ns pubgrub.core
+  (:require [clojure.string :as str]))
 
 ;;;; References:
 ;;;; https://nex3.medium.com/pubgrub-2fb6470504f
@@ -25,6 +26,42 @@
 ;;;; incompatibilities. Through phases of "unit propagation", "conflict resolution", and
 ;;;; "decision making", a solution is hopefully found (complete, partial, or none).
 
+(defn make-version
+  "Make a new version (sequence of integers)."
+  [s]
+  (map #(Integer/parseInt %) (str/split s #"\.")))
+
+(defn extend-version
+  "Extend a version out to N segments."
+  [v n]
+  (let [lv (count v)]
+    (concat v (take (- n lv) (repeat 0)))))
+
+(defn zip-versions
+  "Given two versions, extend them to match lengths and group into pairs."
+  [a b]
+  (let [n (max (count a) (count b))
+        aa (extend-version a n)
+        bb (extend-version b n)]
+    (map vector aa bb)))
+
+(defn equal-versions?
+  "Compare if two versions are equal."
+  [a b]
+  (let [z (zip-versions a b)]
+    (every? #(apply = %) z)))
+
 (comment
+
+  (concat [1 2 3] (take 2 (repeat 0)))
+  (take 4 (repeat 0))
+  (conj [1 2 3] 0)
+  (count [1 2 3])
+
+  (make-version "1.2.3")
+  (extend-version (make-version "1.2.3") 5)
+  (extend-version (make-version "1.2.3") 3)
+  (zip-versions (make-version "1") (make-version "1.2.3"))
+  (equal-versions? (make-version "1") (make-version "1.0.1"))
 
   :rcf)
