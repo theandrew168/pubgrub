d0a10a0400b5db0ae3c55630405bad07e4979a66
theandrew168 <andrew@shallowbrooksoftware.com>
2024-11-10 01:24:13 -0600

    Minor doc string fix

--- src/pubgrub/registry.clj
+++ src/pubgrub/registry.clj
@@ -3,6 +3,6 @@
 (defprotocol Registry
   "Represents a generic package registry."
   (package-versions [this package]
-    "Get all versions (sequence of strings) of a specific package.")
+    "Get all versions (sequence of strings) of a package.")
   (package-version-dependencies [this package version]
     "Get all dependencies (map of string to string, name to term) of a specific package version."))
