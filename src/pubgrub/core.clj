c88ae2a4a7ab4c6b2927f9b84d1ee9e5f3d23878
theandrew168 <andrew@shallowbrooksoftware.com>
2024-11-13 21:03:18 -0600

    Start thinking about the actual alg loop

--- src/pubgrub/core.clj
+++ src/pubgrub/core.clj
@@ -1,5 +1,8 @@
 (ns pubgrub.core
-  (:require [pubgrub.npm :as npm]))
+  (:require
+   [pubgrub.fake :as fake]
+   [pubgrub.registry :as registry]
+   [pubgrub.term :as term]))
 
 ;;;; Let's start with semvers of any length (any number of segments separated by a period).
 ;;;; Ignore prereleases, release candiates, and things like that (for now). So, a version
@@ -24,7 +27,67 @@
 ;;;; main algorithm is categorized as "Conflict-driven clause learning" which relates to
 ;;;; boolean satisfiability.
 
-;;;; Example:
+;; TODO: Create helper for making incompats (set of terms w/ parents?).
+;; TODO: Create func for generating the initial set of incompats.
+;; TODO: Find a better way of details with separate package/version and term parsing.
+
+(defn- add-dep
+  "Given a package and a dependency, add both terms to an incompat with the dep negated."
+  [package version dep range]
+  #{(term/parse (format "%s %s" package version))
+    (assoc (term/parse (format "%s %s" dep range)) :positive? false)})
+
+;; https://github.com/dart-lang/pub/blob/master/doc/solver.md#the-algorithm
+(defn solve
+  "Do the pubgrub thing!"
+  [reg package version]
+  (let [solution {package version}
+        deps (registry/package-version-dependencies reg package version)
+        ;; derive top-level incompats
+        ;;   for each dep, add "{root v, not dep v}"
+        incompatibilities (set (map #(add-dep package version % (deps %)) (keys deps)))]
+    ;; unit propagation
+    ;;   if conflict, try to resolve
+    ;;   if resolution fails, solving has failed (report error)
+    ;; decision making
+    ;;   if no more work to do, solved!
+    ;; repeat til solved or conflict
+    {:solution solution
+     :incompatibilities incompatibilities}))
+
+(comment
+
+  (concat [1 2 3] (take 2 (repeat 0)))
+  (take 4 (repeat 0))
+  (conj [1 2 3] 0)
+  (count [1 2 3])
+
+  ;; https://github.com/dart-lang/pub/blob/master/doc/solver.md#no-conflicts
+  (def example1
+    {"root" {"1.0.0" {"foo" "^1.0.0"}}
+     "foo" {"1.0.0" {"bar" "^1.0.0"}}
+     "bar" {"1.0.0" {}
+            "2.0.0" {}}})
+
+  (def reg1 (fake/new-registry example1))
+  (solve reg1 "root" "1.0.0")
+
+  ;; https://github.com/dart-lang/pub/blob/master/doc/solver.md#avoiding-conflict-during-decision-making
+  (def example2
+    {"root" {"1.0.0" {"foo" "^1.0.0"
+                      "bar" "^1.0.0"}}
+     "foo" {"1.0.0" {}
+            "1.1.0" {"bar" "^2.0.0"}}
+     "bar" {"1.0.0" {}
+            "1.1.0" {}
+            "2.0.0" {}}})
+
+  (def reg2 (fake/new-registry example2))
+  (solve reg2 "root" "1.0.0")
+
+  :rcf)
+
+;;;; Larger Example:
 ;;;; tar 7.4.3 (https://registry.npmjs.org/tar/7.4.3)
 ;;;;   @isaacs/fs-minipass ^4.0.0 (4.0.1)
 ;;;;     minipass ^7.0.4 (7.1.2)
@@ -70,14 +133,3 @@
 ;;;;         package-json-from-dist ^1.0.0 (1.0.1)
 ;;;;   mkdirp ^3.0.1 (3.0.1)
 ;;;;   yallist ^5.0.0 (5.0.0)
-
-(comment
-
-  (concat [1 2 3] (take 2 (repeat 0)))
-  (take 4 (repeat 0))
-  (conj [1 2 3] 0)
-  (count [1 2 3])
-
-  (def reg (npm/new-registry))
-
-  :rcf)
